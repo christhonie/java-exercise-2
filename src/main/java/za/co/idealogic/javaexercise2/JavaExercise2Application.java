34525d724392a825833bd4649a02e3014fe6ee9f
Christhonie Geldenhuys <development@christhonie.co.za>
2020-07-17 15:39:57 +0200

    Added logging

--- src/main/java/za/co/idealogic/javaexercise2/JavaExercise2Application.java
+++ src/main/java/za/co/idealogic/javaexercise2/JavaExercise2Application.java
@@ -1,5 +1,7 @@
 package za.co.idealogic.javaexercise2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaExercise2Application implements CommandLineRunner {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaExercise2Application.class, args);	
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.print("Java Exercise 2: Application started!");
+		log.info("Java Exercise 2: Application started!");
+        log.info("Java Exercise 2: Application ended!");
 	}
 }
